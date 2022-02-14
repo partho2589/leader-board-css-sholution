2aba334c185f5a2774ab4829915eecd729ef78d5
partho2589 <parthomalo615@.com>
2022-02-15 00:14:11 +0600

    color change

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
-console.log('Tray js ')
+// topPlayer color change
+const topPlayer =  document.getElementById('top-player-titel');
+ topPlayer.style.color = 'red';
+
+ // toblogs color change
+ const topBlogs = document.getElementById('top-blogs');
+ topBlogs.style.color = 'red'
+
+ const playersBgChange = document.getElementById('players-name');
+ playersBgChange.style.backgroundColor = 'rgba(255, 0, 0, 0.4)'
